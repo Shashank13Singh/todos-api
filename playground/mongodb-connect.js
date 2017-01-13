6ffb4107bbdd7eebd991119c1619c7499205969c
Shashank Singh <Shashank Singh>
2017-01-13 14:44:49 +0530

    playground file mod

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,21 +1,21 @@
 const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+MongoClient.connect('mongodb://shashank:nfstherun@ds163758.mlab.com:63758/todo_app', (err, db) => {
   if (err) {
     return console.log('Unable to connect to the database');
   }
   console.log('Connected to Mongodb server');
 
-  // db.collection('Todos').insertOne({
-  //   text: 'Something to do',
-  //   completed: false
-  // }, (err, result) => {
-  //   if (err) {
-  //     return console.log('Unable to insert todo', err);
-  //   }
-  //   console.log(JSON.stringify(result.ops, undefined, 2));
-  // });
-  //
+  db.collection('Todos').insertOne({
+    text: 'Something  new to do',
+    completed: false
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert todo', err);
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
   // db.collection('Users').insertOne({
   //   name: 'Sunny',
   //   age: 22,
