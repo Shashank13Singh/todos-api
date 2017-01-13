03b6b38c8d48b8703510a843da4b6fc27ab3a5b3
Shashank Singh <Shashank Singh>
2017-01-13 15:25:55 +0530

    mlab connection added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://shashank:nfstherun@ds163758.mlab.com:63758/todo_app');
+// 'mongodb://shashank:nfstherun@ds163758.mlab.com:63758/todo_app'
+mongoose.connect(process.env.MONGOLAB_URI);
 
 module.exports = {mongoose};
